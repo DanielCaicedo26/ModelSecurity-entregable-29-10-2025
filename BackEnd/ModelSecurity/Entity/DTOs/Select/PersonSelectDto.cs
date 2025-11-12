3b71056493fcbcc688fbe6f6f0a3b096138983b9
User <user@example.com>
2025-11-12 11:24:46 -0500

    subiendo cambios en el dto

--- BackEnd/ModelSecurity/Entity/DTOs/Select/PersonSelectDto.cs
+++ BackEnd/ModelSecurity/Entity/DTOs/Select/PersonSelectDto.cs
@@ -6,6 +6,7 @@ namespace Entity.DTOs.Select
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Identification { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
     }
